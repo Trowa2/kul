e973cd8ede3a45d8ccee6a35c13857a3ffbd76f0
Trowa2 <86346962+Trowa2@users.noreply.github.com>
2021-06-23 13:53:08 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ function sayHello () {
     let myName = prompt('Как тебя завут?');
     let span = document.querySelector('span');
     span.textContent = myName; 
+
 }
 
 let button = document.querySelector('.vp');
